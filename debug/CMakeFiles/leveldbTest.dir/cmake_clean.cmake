1cfcc06aa822708aefccf8309e8a74d2ff79a130
Gooday2die <edina00@naver.com>
2022-08-29 21:22:23 +0900

    Add random inputs

---
+++ debug/CMakeFiles/leveldbTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leveldbTest.dir/dbTest.cpp.o"
+  "CMakeFiles/leveldbTest.dir/dbTest.cpp.o.d"
+  "CMakeFiles/leveldbTest.dir/main.cpp.o"
+  "CMakeFiles/leveldbTest.dir/main.cpp.o.d"
+  "leveldbTest"
+  "leveldbTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leveldbTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,4 +1,27 @@
 #include "dbTest.h"
+#define STRLENGTH 100
+#define ITERCOUNT 100
+
+/**
+ * @brief A function that generates random string.
+ * 
+ * @param len 
+ * @return std::string 
+ */
+std::string gen_random(const int len) {
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    std::string tmp_s;
+    tmp_s.reserve(len);
+
+    for (int i = 0; i < len; ++i) {
+        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+    
+    return tmp_s;
+}
 
 /**
  * @brief A main function for this test code.
@@ -8,9 +31,16 @@
 int main (void) {
     dbTest* db = new dbTest();
 
-    db->putValue("A", "Hello world!");
-    db->putValue("B", "Good bye world!");
-    db->putValue("C", "I am hungry");
+    //db->putValue("A", "Hello world!");
+    //db->putValue("B", "Good bye world!");
+    //db->putValue("C", "I am hungry");
+
+    for (int i = 0 ; i < ITERCOUNT ; i++) {
+        std::string curIndex = std::to_string(i);
+        std::string randomString = gen_random(STRLENGTH);
+
+        db->putValue(curIndex, randomString);
+    }
 
     delete(db);
     return 0;
